e8a866b467b13443ff2f1798231aa9152e827261
Diana <diana.barnea@gmail.com>
2020-11-30 17:52:00 +0200

    adding controller tests

--- src/test/java/diana/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
+++ src/test/java/diana/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -3,6 +3,7 @@ package diana.springframework.msscbeerservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//@RunWith(SpringRunner.class) gets eliminated because it belongs to Junit4
 @SpringBootTest
 class MsscBeerServiceApplicationTests {
 
