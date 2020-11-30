6459916229d20f17b40230e790f932f139c53891
Diana <diana.barnea@gmail.com>
2020-11-30 16:46:10 +0200

    adding web data model

---
+++ src/main/java/diana/springframework/msscbeerservice/web/model/BeerPagedList.java
@@ -0,0 +1,20 @@
+package diana.springframework.msscbeerservice.web.model;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+// PageImpl<T> is from spring-boot-data-jpa dependency
+// BeerPageList will be used for List Beers service
+public class BeerPagedList extends PageImpl<BeerDto> {
+
+    public BeerPagedList(List<BeerDto> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPagedList(List<BeerDto> content) {
+        super(content);
+    }
+}
