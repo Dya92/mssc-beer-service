143e9f34fc0987b255ed3dafdd806646805bcbf4
Diana <diana.barnea@gmail.com>
2020-12-04 02:02:33 +0200

    added mapper for beer objects

---
+++ src/main/java/diana/springframework/msscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,11 @@
+package diana.springframework.msscbeerservice.web.mappers;
+
+import diana.springframework.msscbeerservice.domain.Beer;
+import diana.springframework.msscbeerservice.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = DateMapper.class)
+public interface BeerMapper {
+    BeerDto beerToBeerDto(Beer beer);
+    Beer beerDtoToBeer(BeerDto beerDto);
+}
